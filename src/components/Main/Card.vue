f88ff0e9145d69494b4e1b560bb9222bc709b0fc
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-27 16:18:53 +0200

    CardContainer layout

---
+++ src/components/Main/Card.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="image">
+            <img src="https://picsum.photos/id/1/200/300" alt="">
+        </div>
+        <h2>Card Name</h2>
+        <div class="race">Race</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/partials/variables' as *;
+
+.card {
+    background-color: $primary-color;
+    flex-basis: 20%;
+    margin: 0 $cards-margin $cards-margin;
+    text-align: center;
+
+    h2 {
+        color: $secondary-color;
+        margin: 1.875rem 1.25rem;
+    }
+
+    .race {
+        color: $tertiary-color;
+        margin-bottom: 1.875rem;
+    }
+}
+</style>
--- src/components/Main/CardContainer.vue
+++ src/components/Main/CardContainer.vue
@@ -1,13 +1,49 @@
 <script>
+import Card from './Card.vue';
+
 export default {
-    name: 'CardContainer'
+    name: 'CardContainer',
+    components: {
+        Card
+    }
 }
 </script>
 
 <template>
     <div class="small-container">
         <div class="smaller-container">
-            Ciao
+            <div class="found-cards">
+                Found 39 cards
+            </div>
+            <div class="cards">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+@use '../../assets/styles/partials/variables' as *;
+
+.small-container {
+    background-color: $secondary-color;
+    padding: 35px;
+
+    .found-cards {
+        background-color: $tertiary-color;
+        color: $secondary-color;
+        font-weight: 600;
+        padding: 15px;
+    }
+
+    .cards {
+        display: flex;
+        justify-content: space-between;
+        margin: 0 (-$cards-margin);
+    }
+}
+</style>
